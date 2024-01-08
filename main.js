b750cdf7e97b9ed25a48e5492c13f3d7e394381d
DomenicoScala <dome.scala12@gmail.com>
2024-01-08 01:06:57 +0100

    FINE ESERCIZIO

--- main.js
+++ main.js
@@ -55,3 +55,64 @@ const posts = [
         "created": "2021-03-05"
     }
 ];
+
+
+
+const postContainer = document.getElementById('container');
+
+posts.forEach((post, i, arr) =>{
+    postContainer.innerHTML += `
+        <div class="post">
+            <div class="post__header">
+                <div class="post-meta">                    
+                    <div class="post-meta__icon">
+                        <img class="profile-pic" src="${post.author.image}" alt="Phil Mangione">                    
+                    </div>
+                    <div class="post-meta__data">
+                        <div class="post-meta__author">${post.author.name}</div>
+                        <div class="post-meta__time">${post.created}</div>
+                    </div>                    
+                </div>
+            </div>
+            <div class="post__text">${post.content}</div>
+            <div class="post__image">
+                <img src="${post.media}" alt="">
+            </div>
+            <div class="post__footer">
+                <div class="likes js-likes">
+                    <div class="likes__cta">
+                        <a class="like-button  js-like-button" data-postid="1">
+                            <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                            <span class="like-button__label">Mi Piace</span>
+                        </a>
+                    </div>
+                    <div class="likes__counter">
+                        Piace a <b id="like-counter-${post.id}" class="js-likes-counter">${post.likes}</b> persone
+                    </div>
+                </div> 
+            </div>            
+        </div>
+    `
+})  
+
+const likeButton = document.querySelectorAll('.like-button');
+
+const likedPost = [];
+
+
+
+likeButton.forEach((clickLikeButton,i) => {
+    clickLikeButton.addEventListener('click', function(){
+        
+        if(!likedPost.includes(posts[i].id)){
+            this.classList.add('like-button--liked');
+    
+            likedPost.push(posts[i].id);
+    
+            const idLike = document.getElementById('like-counter-' + posts[i].id);
+    
+            const likesNumber = parseInt(idLike.innerText);
+            idLike.innerText = likesNumber + 1;
+        }                
+    })
+})
